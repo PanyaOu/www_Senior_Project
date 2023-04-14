adb43a7189ec59b2976040e0966465ca9c28a503
PanyaOu <75552913+PanyaOu@users.noreply.github.com>
2023-04-13 23:47:04 -0700

    outline

--- assets/js/introduction.js
+++ assets/js/introduction.js
@@ -1,6 +1,7 @@
 
 //Fetches data from a google docs and populates a page on the index.html
 //ALL FORMATTING FROM GOOGLE DOCS IS SAVED. This is a current issue.
+/*
 async function fetchGoogleDocContent() {
   const googleDocUrl = 'https://docs.google.com/document/d/e/2PACX-1vQg5TRFar7qpB_ky6CURWWXMoQBZe_c-M11DKLGnBAjcCXfhebhAs3zxzQup3q84-ZetCIhVlF6wZEs/pub';
   const response = await fetch(googleDocUrl);
@@ -9,12 +10,12 @@ async function fetchGoogleDocContent() {
   const doc = parser.parseFromString(html, 'text/html');
   const content = doc.querySelector('#contents');
   document.getElementById('google-doc-content').innerHTML = content.innerHTML;
-}
+}*/
 
 /*
 This function grabs the image and text seperately without the formatting.
 I have yet to get this to work as the text contains css that I do not yet 
-know how to remove.
+know how to remove. */
 
 async function fetchGoogleDocContent() {
   const googleDocUrl = 'https://docs.google.com/document/d/e/2PACX-1vQg5TRFar7qpB_ky6CURWWXMoQBZe_c-M11DKLGnBAjcCXfhebhAs3zxzQup3q84-ZetCIhVlF6wZEs/pub';
@@ -24,11 +25,15 @@ async function fetchGoogleDocContent() {
   const doc = parser.parseFromString(html, 'text/html');
   const content = doc.querySelector('#contents');
 
-  // Fetch plain text
-  const textContent = content.innerText;
-  document.getElementById('google-doc-content').innerText = textContent;
+  console.log(content);
 
-  console.log(textContent);
+
+  // Fetch plain text
+  let textElements  = content.querySelectorAll("p, h1, h2, h3, h4, h5, h6");
+  let textContent = Array.from(textElements).map(element => element.textContent);
+  let allText = textContent.join("\n");
+
+  document.getElementById('google-doc-content').innerText = allText;
 
   // Fetch images
   const images = content.querySelectorAll('img');
@@ -39,7 +44,6 @@ async function fetchGoogleDocContent() {
     imageContainer.appendChild(newImg);
   });
 }
-*/
 
 //fetches information from a google spreadsheet and makes them into links to populate
 //the introduction page.
